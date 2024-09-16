ab4c7fba7e2c5803b55b401807858704bfce219f
Bruna Guarizo <brunaguarizo@Brunas-MacBook-Pro.local>
2024-09-16 11:24:23 -0700

    Adjusted

--- LabActivity.js
+++ LabActivity.js
@@ -1,12 +1,17 @@
 const program = "college"
 let classes = "courses"
-console.log ("I’m starting to study in a " + program + " here in Canada, so my weekly past time activities were understand everything about this " + program + ", such as where to get to the " + program + ", which bus I should get, and also the location and time for each " + classes + ".");
-/*Cesaria's : This looks really good , the country could actually 
-be used a variable that changeable for future changes*/
+
+console.log ("I’m starting to study in a " + program + " here in Canada, so my weekly past time activities were understand everything about this " + program + ", such as where to get to the " + program + ", which bus I should get, and also the location and time for each " + classes + ". In this " + program + ", I have " + 8 + " different " + classes + ". The number of the " + classes + " are big, so I need to pay attention in everything so I don’t get lost in the " + program + ".");
 
 /* need to write 
 
 multiple console logs */
+
+console.log ("______________________________REVIEW ONE______________________________");
+
+console.log ("I’m starting to study in a " + program + " here in Canada, so my weekly past time activities were understand everything about this " + program + ", such as where to get to the " + program + ", which bus I should get, and also the location and time for each " + classes + ".");
+/*Cesaria's : This looks really good , the country could actually 
+be used a variable that changeable for future changes*/
 
 classes = "D3"
 

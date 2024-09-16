c8f72ca00c7000f62e9371284245ed0a0521d785
Bruna Guarizo <brunaguarizo@Brunas-MacBook-Pro.local>
2024-09-16 10:45:34 -0700

    LabActivityWeek3

---
+++ LabActivity.js
@@ -0,0 +1,11 @@
+const program = "college"
+let classes = "courses"
+console.log ("I’m starting to study in a " + program + " here in Canada, so my weekly past time activities were understand everything about this " + program + ", such as where to get to the " + program + ", which bus I should get, and also the location and time for each " + classes + ".");
+
+/* need to write 
+
+multiple console logs */
+
+classes = "D3"
+
+console.log("In this " + program + ", I have " + 8 + " different " + classes + ". The number of the " + classes + " are big, so I need to pay attention in everything so I don’t get lost in the " + program + ".");

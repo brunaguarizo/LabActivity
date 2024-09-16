e24b6e20bfc6b6c48f3b0364911284b1218fa7d7
Bruna Guarizo <brunaguarizo@Brunas-MacBook-Pro.local>
2024-09-16 11:16:55 -0700

    LabActivityWeek3

--- LabActivity.js
+++ LabActivity.js
@@ -1,6 +1,8 @@
 const program = "college"
 let classes = "courses"
 console.log ("I’m starting to study in a " + program + " here in Canada, so my weekly past time activities were understand everything about this " + program + ", such as where to get to the " + program + ", which bus I should get, and also the location and time for each " + classes + ".");
+/*Cesaria's : This looks really good , the country could actually 
+be used a variable that changeable for future changes*/
 
 /* need to write 
 
@@ -9,3 +11,22 @@ multiple console logs */
 classes = "D3"
 
 console.log("In this " + program + ", I have " + 8 + " different " + classes + ". The number of the " + classes + " are big, so I need to pay attention in everything so I don’t get lost in the " + program + ".");
+/* Cesaria: You could create a variable for the number 8 as well in case you would like to change it later when you explore more countries*/
+
+console.log("______________________________REVIEW TWO______________________________");
+
+
+classes = "courses"
+let country = "Canada"
+let number ="8"
+
+console.log ("I’m starting to study in a " +program+ " here in " +country+ ", so my weekly past time activities were understand everything about this " + program + ", such as where to get to the " +program+ ", which bus I should get, and also the location and time for each " +classes+ ".");
+
+classes = "D3"
+
+console.log("In this " +program+ ", I have " +number+ " different " +classes+ ". The number of the " +classes+ " are big, so I need to pay attention in everything so I don’t get lost in the " +program+ ".");
+
+country = "Portugal"
+number = "4"
+
+console.log ("When I was studying in " +country+  ", my schedule weren't that full, I just had " +number+ " hours courses per week.");
